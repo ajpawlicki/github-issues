401408b68ebe64de020f9d1fa7b51bc91b6d5dc3
AJ Pawlicki <aloysiuspaw@gmail.com>
2018-02-16 11:59:23 -0800

    Create Issue component

---
+++ react-client/src/components/Issue/Issue.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+import './issue-style.css';
+
+export default (props) => {
+  const { number, user, state, title, assignee, labels, created_at, html_url } = props.issue;
+  const date = new Date(created_at).toDateString();
+
+  return (
+    <div className="issue-container">
+      <div className="issue-content-col">
+        <div className="issue-title-container">
+          <span className="issue-title">
+            <a href={html_url} target="_blank">{title}</a>
+          </span>
+
+          {labels.map((label, index) => {
+            return (
+              <span key={index} style={{ backgroundColor: '#' + label.color }} className="issue-label">
+                {label.name}
+              </span>
+            );
+          })}
+
+        </div>
+
+        <div className="issue-description">
+          <span>
+            {`#${number} ${state}ed on ${date} by ${user.login}`}
+          </span>
+        </div>
+      </div>
+
+      <div className="issue-thumbnail-col">
+        { assignee ? <img src={assignee.avatar_url} alt="" height="20" width="20"/> : null }
+      </div>
+    </div>
+  );
+};
