e68aee114fb31657eacad88a6f38a6dc9dc5cf4d
AJ Pawlicki <aloysiuspaw@gmail.com>
2018-02-16 11:59:39 -0800

    Add style and css loader to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,8 @@ module.exports = {
         query: {
           presets: ['react', 'env']
         }
-      }
+      },
+      { test: /\.css$/, loader: 'style-loader!css-loader' },
     ]
   }
 };
